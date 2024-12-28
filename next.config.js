2d3b3967884b8d0b7b4ed270f7663fd052bb5d2e
Quincybob <74205675+Quincybob@users.noreply.github.com>
2024-12-28 20:40:25 +0700

    debugggg

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "gateway.pinata.cloud",
-      },
-    ],
+    domains: ["gateway.pinata.cloud"],
+    unoptimized: true,
+  },
+  env: {
+    PINATA_JWT: process.env.PINATA_JWT,
+    PINATA_GROUP_ID: process.env.PINATA_GROUP_ID,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
